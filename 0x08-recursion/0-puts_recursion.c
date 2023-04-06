01d1508f9e61b3d07b6708a99a4ab1ec44698784
Ismail Bensghir <ismsge@gmail.com>
2023-04-06 16:43:56 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,17 @@
-#include "main.h"
+[200~#include "main.h"
 /**
- * _puts_recursion - function like puts();
- * @s: input
- * Return: Always 0 (Success)
- */
+ *  * _puts_recursion - function like puts();
+ *   * @s: input
+ *    * Return: Always 0 (Success)
+ *     */
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
+		if (*s)
+				{
+							_putchar(*s);
+									_puts_recursion(s + 1);
+										}
 
-	else
-		_putchar('\n');
+			else
+						_putchar('\n');
 }
